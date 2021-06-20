b0e090cb2ed71b83e9fb7a8e3a2885dde3239f31
jtj3107 <jtj3926@gmail.com>
2021-06-20 14:00:15 +0900

    DB연결후 쿼리결과출력

---
+++ db.sql
@@ -0,0 +1,26 @@
+DROP DATABASE IF EXISTS mysqlutil;
+CREATE DATABASE mysqlutil;
+USE mysqlutil;
+
+CREATE TABLE article (
+    id INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY(id),
+    regDate DATETIME NOT NULL,
+    updateDate DATETIME NOT NULL,
+    title CHAR(100) NOT NULL,
+    `body` TEXT NOT NULL
+);
+
+INSERT INTO article
+SET regDate =  NOW(),
+updateDate = NOW(),
+title = '제목1',
+`body` = '내용1';
+
+INSERT INTO article
+SET regDate =  NOW(),
+updateDate = NOW(),
+title = '제목2',
+`body` = '내용2';
+
+SELECT * FROM article;
